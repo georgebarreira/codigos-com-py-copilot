4a104302abb85cae0a7400a3916cf193ea7757b1
George Barreira <98276897+georgebarreira@users.noreply.github.com>
2024-12-06 06:55:34 -0300

    Alterando concat

--- concat_dados.py
+++ concat_dados.py
@@ -1,1 +1,5 @@
 # Receber 2 dados diferentes do usuário e concatena-los em python
+valor1 = input("Digite o primeiro valor: ")
+valor2 = input("Digite o segundo valor: ")
+resultado = valor1 + " " + valor2
+print("A concatenação dos valores é: ", resultado)
